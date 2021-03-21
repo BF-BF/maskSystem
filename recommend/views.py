34bbaaf84edca617419584f0443dbb0b59449e25
Sukki <1163046864@qq.com>
2021-03-21 10:33:54 +0800

    调试推荐views

--- recommend/views.py
+++ recommend/views.py
@@ -10,19 +10,23 @@ def recommend():
     all_can_mask_profile = get_train_data.createMaskProfile()
 
     can_cos = []
-    for x in range(len(get_train_data.get_all_topic())):
+    for x in range(len(can_mask_name)):
         can_cos.append(get_train_data.cos_sim(user_profile, all_can_mask_profile[x]))
 
     mask_cos = dict()
     for x in range(len(can_mask_name)):
-        mask_cos[can_cos] = can_mask_name[x]
+        mask_cos[can_mask_name[x]] = can_cos[x]
+    print(mask_cos)
 
-    score = mask_cos.keys()
-    score.sort(reversed=True)
+    score = list(mask_cos.values())
+    score.sort(reverse=True)
+    print(score[:3])
+    print(list(mask_cos.keys()))
+
     recommend_mask_name = []
-    for key in mask_cos.keys():
-        if key in score[:3]:
-            recommend_mask_name.append(mask_cos[key])
+    for value in list(mask_cos.values()):
+        if value in score[:3]:
+            recommend_mask_name.append(list(mask_cos.keys())[list(mask_cos.values()).index(value)])
     print(recommend_mask_name)
 
     # return JsonResponse(recommend_mask_name)
